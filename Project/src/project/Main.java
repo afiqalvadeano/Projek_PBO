a06c037369e4d6861e9aaae3461001c7b753da47
afiqalvadeano <56910906+afiqalvadeano@users.noreply.github.com>
2020-05-23 10:58:03 +0700

    Add files via upload

--- Project/src/project/Main.java
+++ Project/src/project/Main.java
@@ -2,6 +2,11 @@ package project;
 
 import java.util.*;
 import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.RandomAccessFile;
 import javax.swing.JFrame;
 
 public class Main {
@@ -12,6 +17,7 @@ public class Main {
         int pilih;
         char kembali;
         Scanner input = new Scanner(System.in);
+        RandomAccessFile fSumber = null;
 
         do {
             System.out.println("=====MENU=====");
@@ -102,6 +108,11 @@ public class Main {
                     double lKalaRevMerkurius, lKalaRevVenus, lKalaRevBumi, lKalaRevMars, lKalaRevJupiter, lKalaRevSaturnus, lKalaRevUranus, lKalaRevNeptunus;
                     double lKalaRoMerkurius, lKalaRoVenus, lKalaRoBumi, lKalaRoMars, lKalaRoJupiter, lKalaRoSaturnus, lKalaRoUranus, lKalaRoNeptunus;
 
+                    try {
+                    String file = "E:\\UPN\\SMT 4\\PBO\\Project\\coba.txt";
+                    fSumber = new RandomAccessFile(file, "rw");
+                    
+                    if (fSumber.length() < 1) {
                     System.out.println("--- Merkurius ---");
                     System.out.print("Masukkan Jari-Jari Merkurius : ");
                     rMerkurius = input.nextDouble();
@@ -111,6 +122,11 @@ public class Main {
                     lKalaRoMerkurius = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevMerkurius = input.nextDouble();
+                    
+                    fSumber.writeDouble(rMerkurius);
+                        fSumber.writeDouble(linRevMerkurius);
+                        fSumber.writeDouble(lKalaRoMerkurius);
+                        fSumber.writeDouble(lKalaRevMerkurius);
 
                     Merkurius satu = new Merkurius(rMerkurius, linRevMerkurius, lKalaRoMerkurius, lKalaRevMerkurius, pt);
 
@@ -123,6 +139,11 @@ public class Main {
                     lKalaRoVenus = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevVenus = input.nextDouble();
+                    
+                    fSumber.writeDouble(rVenus);
+                        fSumber.writeDouble(linRevVenus);
+                        fSumber.writeDouble(lKalaRoVenus);
+                        fSumber.writeDouble(lKalaRevVenus);
 
                     Venus dua = new Venus(rVenus, linRevVenus, lKalaRoVenus, lKalaRevVenus, pt);
 
@@ -135,6 +156,11 @@ public class Main {
                     lKalaRoBumi = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevBumi = input.nextDouble();
+                    
+                    fSumber.writeDouble(rBumi);
+                        fSumber.writeDouble(linRevBumi);
+                        fSumber.writeDouble(lKalaRoBumi);
+                        fSumber.writeDouble(lKalaRevBumi);
 
                     Bumi tiga = new Bumi(rBumi, linRevBumi, lKalaRoBumi, lKalaRevBumi, pt);
 
@@ -147,6 +173,11 @@ public class Main {
                     lKalaRoMars = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevMars = input.nextDouble();
+                    
+                    fSumber.writeDouble(rMars);
+                        fSumber.writeDouble(linRevMars);
+                        fSumber.writeDouble(lKalaRoMars);
+                        fSumber.writeDouble(lKalaRevMars);
 
                     Mars empat = new Mars(rMars, linRevMars, lKalaRoMars, lKalaRevMars, pt);
 
@@ -159,6 +190,11 @@ public class Main {
                     lKalaRoJupiter = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevJupiter = input.nextDouble();
+                    
+                    fSumber.writeDouble(rJupiter);
+                        fSumber.writeDouble(linRevJupiter);
+                        fSumber.writeDouble(lKalaRoJupiter);
+                        fSumber.writeDouble(lKalaRevJupiter);
 
                     Jupiter lima = new Jupiter(rJupiter, linRevJupiter, lKalaRoJupiter, lKalaRevJupiter, pt); // Belum ada classnya
 
@@ -171,6 +207,11 @@ public class Main {
                     lKalaRoSaturnus = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevSaturnus = input.nextDouble();
+                    
+                    fSumber.writeDouble(rSaturnus);
+                        fSumber.writeDouble(linRevSaturnus);
+                        fSumber.writeDouble(lKalaRoSaturnus);
+                        fSumber.writeDouble(lKalaRevSaturnus);
 
                     Saturnus enam = new Saturnus(rSaturnus, linRevSaturnus, lKalaRoSaturnus, lKalaRevSaturnus, pt); // Belum ada classnya
 
@@ -183,6 +224,11 @@ public class Main {
                     lKalaRoUranus = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevUranus = input.nextDouble();
+                    
+                    fSumber.writeDouble(rUranus);
+                        fSumber.writeDouble(linRevUranus);
+                        fSumber.writeDouble(lKalaRoUranus);
+                        fSumber.writeDouble(lKalaRevUranus);
 
                     Uranus tujuh = new Uranus(rUranus, linRevUranus, lKalaRoUranus, lKalaRevUranus, pt); // Belum ada classnya
 
@@ -195,6 +241,11 @@ public class Main {
                     lKalaRoNeptunus = input.nextDouble();
                     System.out.print("Masukkan Kala Revolusi              : ");
                     lKalaRevNeptunus = input.nextDouble();
+                    
+                    fSumber.writeDouble(rNeptunus);
+                        fSumber.writeDouble(linRevNeputunus);
+                        fSumber.writeDouble(lKalaRoNeptunus);
+                        fSumber.writeDouble(lKalaRevNeptunus);
 
                     Neptunus delapan = new Neptunus(rNeptunus, linRevNeputunus, lKalaRoNeptunus, lKalaRevNeptunus, pt); // Belum ada classnya
 
@@ -214,16 +265,51 @@ public class Main {
                     tujuh.thread.setPriority(5);
                     delapan.thread.start();
                     delapan.thread.setPriority(4);
+                    }else{
+                        System.out.println("Raf Leng : " + fSumber.length());
 
-                    IlustrasiVolume l = new IlustrasiVolume();
-                    l.setVisible(true);
-                    JFrame myFrame = new JFrame("Hello");
-                    myFrame.add(l);
-                    myFrame.pack();
-                    myFrame.setVisible(true);
-                    myFrame.setSize(800, 800);
+                        fSumber.seek(0);
+                        int i = 0;
+                        double planet[][] = new double[8][4];
+                        while (fSumber.getFilePointer() < fSumber.length()) {
+                            for (int j = 0; j < 4; j++) {
+                                planet[i][j] = fSumber.readDouble();
+                            }
+                            i++;
+                        }
 
-                    l.init();
+                        String namaPlanet[] = {"MERKURIUS", "VENUS", "BUMI", "MARS", "JUPITER", "SATURNUS", "URANUS", "NEPTUNUS"};
+                        for (int a = 0; a < 8; a++) {
+                            System.out.println(namaPlanet[a] + "\n");
+                            System.out.println("Jari jari " + namaPlanet[a].toLowerCase() + " : " + planet[a][0]);
+                            System.out.println("lintasan " + namaPlanet[a].toLowerCase() + " : " + planet[a][1]);
+                            System.out.println("rotasi " + namaPlanet[a].toLowerCase() + " : " + planet[a][2]);
+                            System.out.println("revolusi " + namaPlanet[a].toLowerCase() + " : " + planet[a][3]);
+                            System.out.println("");
+
+                        }
+                        Merkurius satu = new Merkurius(planet[0][0], planet[0][1], planet[0][2], planet[0][3], pt);
+                        Venus dua = new Venus(planet[1][0], planet[1][1], planet[1][2], planet[1][3], pt);
+                        Bumi tiga = new Bumi(planet[2][0], planet[2][1], planet[2][2], planet[2][3], pt);
+                        Mars empat = new Mars(planet[3][0], planet[3][1], planet[3][2], planet[3][3], pt);
+                        Jupiter lima = new Jupiter(planet[4][0], planet[4][1], planet[4][2], planet[4][3], pt);
+                        Saturnus enam = new Saturnus(planet[5][0], planet[5][1], planet[5][2], planet[5][3], pt);
+                        Uranus tujuh = new Uranus(planet[6][0], planet[6][1], planet[6][2], planet[6][3], pt);
+                        Neptunus delapan = new Neptunus(planet[7][0], planet[7][1], planet[7][2], planet[7][3], pt);
+
+                        satu.thread.start();
+                        dua.thread.start();
+                        tiga.thread.start();
+                        empat.thread.start();
+                        lima.thread.start();
+                        enam.thread.start();
+                        tujuh.thread.start();
+                        delapan.thread.start();
+                    }
+                    fSumber.close();
+                    }catch (Exception e) {
+                    System.out.println(e);
+                }
                     break;
                 }
                 case 3: {
